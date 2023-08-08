9c95dd63f945ee2662a98428fe5265a6136c02b9
TruongNBN <truongnbn.main@gmail.com>
2023-08-08 21:08:09 +0700

    Add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    /** Rule Eslint */
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'import', 'jsx-a11y', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  'indent': ['error', 2], // Căn tab 2 spaces
+  // 'quotes': ['error', 'single'], // Sử dụng ngoặc '' và ""
 }
