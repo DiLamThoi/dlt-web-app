28b758630bd2147e83e4fd767743f71c804405b1
TruongNBN <mylove.lanhuong@gmail.com>
2023-08-11 23:08:59 +0700

    add fakeData

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../../dlt-module-commons/dlt-object-base/root/reducer/rootReducer';
 import rootSaga from '../../dlt-module-commons/dlt-object-base/root/saga/rootSaga';
+import fakeData from './fakeData.json'
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState: fakeData,
   middleware: [...getDefaultMiddleware(), sagaMiddleware]
 });
 
