76181ec71c658dfcaf8edf54be3d21affa3025ae
TruongNBN <truongnbn.main@gmail.com>
2023-10-28 02:44:50 +0700

    add ./script/

---
+++ scripts/clone.ps1
@@ -0,0 +1,8 @@
+# Di chuyển ra thư mục gốc
+Set-Location -Path ..
+
+# Địa chỉ URL của kho git: dlt-module-commons
+$repos = "https://github.com/DiLamThoi/dlt-module-commons.git"
+
+# Thực hiện git clone
+git clone $repos
